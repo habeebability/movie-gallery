1a2dabf629a847fdb35736b2828c1e3eb0d904ff
HABEEB AWOYEMI <awoyemi.habeeb@gmail.com>
2020-12-12 20:35:58 +0100

    movie search practice with api

---
+++ index.js
@@ -0,0 +1,65 @@
+const fetchData = async (searchTerm) => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '599fe31b',
+      s: searchTerm,
+    }
+  });
+  if (response.data.Error) {
+    return [];
+  }
+return response.data.Search; 
+};
+
+createAutoComplete({
+  root: document.querySelector('.autocomplete')
+});
+
+
+const onMovieSelect = async movie => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '599fe31b',
+      i: movie.imdbID,
+    }
+  });
+document.querySelector('#summary').innerHTML = movieTemplate(response.data) ; 
+}
+const movieTemplate = (movieDetail) => {
+  return `
+    <article class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img src="${movieDetail.Poster}">
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        </div>
+      </div>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.Awards}</p>
+      <p class="subtitle">Awards</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.BoxOffice}</p>
+      <p class="subtitle">Box Office</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.Metascore}</p>
+      <p class="subtitle">Meta Score</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.imdbRating}</p>
+      <p class="subtitle">IMDB Rating</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.imdbVotes}</p>
+      <p class="subtitle">IMDB VOTES</p>
+    </article>
+  `;
+}
